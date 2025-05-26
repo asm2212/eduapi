2ec1af3ee00af8fc3d8463928b2178710896159d
asm2212 <asmareadmasu0@gmail.com>
2025-05-26 05:13:39 +0300

    feat: add colorette and source-map-support for enhanced logging and error tracing

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,34 @@
 import util from 'util'
 import { createLogger, format, transports } from 'winston'
-import { ConsoleTransportInstance } from 'winston/lib/winston/transports'
+import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports'
 import config from '../configs/config'
 import { EApplicationEnvironment } from '../constants/application'
+import path from 'path'
+import * as sourceMapSupport from 'source-map-support'
+import { blue, green, magenta, red, yellow } from 'colorette'
+
+// Linking Trace Support
+sourceMapSupport.install()
+
+const colorizeLevel = (level: string) => {
+    switch (level) {
+        case 'ERROR':
+            return red(level)
+        case 'INFO':
+            return blue(level)
+        case 'WARN':
+            return yellow(level)
+        default:
+            return level
+    }
+
+
+}
 
 const consoleLogFormat = format.printf((info) => {
     const { level, message, timestamp, meta = {} } = info
-    const customLevel = level.toUpperCase()
-    const customTimestamp = timestamp as string
+    const customLevel = colorizeLevel(level.toUpperCase())
+    const customTimestamp = green(timestamp as string)
     const customMessage = message as string
     const customMeta = util.inspect(meta, {
         showHidden: false,
@@ -15,7 +36,7 @@ const consoleLogFormat = format.printf((info) => {
         colors: true
     })
 
-    const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n${customMeta}\n`
+    const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n${magenta('META')}${customMeta}\n`
     return customLog
 })
 
@@ -33,10 +54,51 @@ const consoleTransport = (): Array<ConsoleTransportInstance> => {
     return []
 }
 
-export default createLogger({
+const fileLogFormat = format.printf((info) => {
+  
+    const { level, message, timestamp, meta = {} } = info
 
+    const logMeta: Record<string, unknown> = {}
+
+    // Type guard to ensure meta is an object
+    if (typeof meta === 'object' && meta !== null) {
+        for (const [key, value] of Object.entries(meta)) {
+            if (value instanceof Error) {
+                logMeta[key] = {
+                    name: value.name,
+                    message: value.message,
+                    trace: value.stack || ''
+                }
+            } else {
+                logMeta[key] = value
+            }
+        }
+    } else {
+        // Handle the case where meta is not an object (optional)
+        logMeta['meta_was_not_an_object'] = meta
+    }
+    const logData = {
+        level: level.toUpperCase(),
+        message,
+        timestamp,
+        meta: logMeta
+    }
+
+    return JSON.stringify(logData, null, 4)
+})
+const FileTransport = (): Array<FileTransportInstance> => {
+    return [
+        new transports.File({
+            filename: path.join(__dirname, '../', '../', 'logs', `${config.ENV}.log`),
+            level: 'info',
+            format: format.combine(format.timestamp(), fileLogFormat)
+        })
+    ]
+}
+
+export default createLogger({
     defaultMeta: {
         meta: {}
     },
-    transports: [...consoleTransport()]
+     transports: [...FileTransport(), ...consoleTransport()]
 })
