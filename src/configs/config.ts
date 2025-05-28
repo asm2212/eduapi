4ba1e4e866d37413f3dc73d0e29987bb1312209b
asm2212 <asmareadmasu0@gmail.com>
2025-05-28 10:44:05 +0300

    feat: add token generation and verification utilities, including payload extensions and environment variable checks

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,13 +1,19 @@
-import dotenvFlow from 'dotenv-flow'
-dotenvFlow.config()
+import dotenvFlow from 'dotenv-flow';
+dotenvFlow.config();
 
 export default {
- // General
+    // General
     ENV: process.env.ENV,
     PORT: process.env.PORT,
     SERVER_URL: process.env.SERVER_URL,
 
     // Database
-    DATABASE_URL: process.env.DATABASE_URL
+    DATABASE_URL: process.env.DATABASE_URL,
 
-}
+    // Tokens
+
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+    ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
+    REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY
+};
